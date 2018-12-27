6da35cec6bdea7d38b4e185195c29bc8f646cd01
Hyobin Kim <gyqls2233@gmail.com>
2018-12-27 14:52:24 +0900

    report card: Print grades of each subject

--- report_card.c
+++ report_card.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void)
+int main()
 {
+	int kor, eng, math;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+
 	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
 	return 0;
 }
